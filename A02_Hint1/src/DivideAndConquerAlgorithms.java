aa17cefefeff4fc3f64e873c93f532fa0ff88e27
JohnnyB87 <johnbrady1987@gmail.com>
2017-11-03 19:58:50 +0000

    start

---
+++ A02_Hint1/src/DivideAndConquerAlgorithms.java
@@ -0,0 +1,172 @@
+
+/**
+ * The class contains the Divide and Conquer-based Algorithms we are using. 
+ */
+public class DivideAndConquerAlgorithms {
+
+	//----------------------------------------------
+	// Class constructor
+	//----------------------------------------------	
+	/**
+	 * Constructor of the class. Do not edit it.
+	 */
+	public DivideAndConquerAlgorithms(){}
+		
+	//-------------------------------------------------------------------
+	// 0. iterativeDisplayElements --> Displays all elements of a MyList 
+	//-------------------------------------------------------------------	
+	/**
+	 * Given a concrete MyList, this iterative algorithm displays its elements by screen (if any).
+	 * @param m: The MyList we want to display its elements.	  
+	 */	
+	public void iterativeDisplayElements(MyList<Integer> m){
+		//-----------------------------
+		//SET OF OPS
+		//-----------------------------
+
+		//-----------------------------
+		// I. SCENARIO IDENTIFICATION
+		//-----------------------------
+		int scenario = 0; 
+		
+		//Rule 1. MyList is empty
+		if (m.length() == 0) 
+			scenario = 1;
+		//Rule 2. MyList is non-empty
+		else
+			scenario = 2;
+
+		//-----------------------------
+		// II. SCENARIO IMPLEMENTATION 
+		//-----------------------------
+		switch(scenario){	
+				
+		//Rule 1. MyList is empty
+		case 1: 
+			//1. We print the empty message
+			System.out.println("Empty MyList");
+			
+			break;
+			
+		//Rule 2. MyList is non-empty
+		case 2: 
+			//1. We print the initial message
+			int size = m.length();
+			System.out.println("MyList Contains the following " + size + " items: ");
+			
+			//2. We traverse the items
+			for (int i = 0; i < size; i++)
+				System.out.println("Item " + i + ": " + m.getElement(i));
+			
+			break;
+	
+		}
+		
+	}
+
+	//-------------------------------------------------------------------
+	// 1. maxInt --> Computes the maximum item of MyList 
+	//-------------------------------------------------------------------	
+	/**
+	 * The function computes the maximum item of m (-1 if m is empty). 
+	 * @param m: The MyList we want to compute its maximum item.
+	 * @return: The maximum item of MyList	  
+	 */	
+	public int maxInt(MyList<Integer> m){	
+		if(m.length() == 0)
+			return -1;
+		int n = m.getElement(0);
+		m.removeElement(0);
+		int num = maxInt(m);
+		m.addElement(0, n);
+		return num > n ? num : n;
+	}
+
+	//-------------------------------------------------------------------
+	// 2. isReverse --> Computes if MyList is sorted in decreasing order 
+	//-------------------------------------------------------------------	
+	/**
+	 * The function computes whether m is sorted in decreasing order or not.  
+	 * @param m: The MyList we want to check.
+	 * @return: Whether m is sorted in decreasing order or not.  
+	 */	
+	public boolean isReverse(MyList<Integer> m){
+
+		if(m.length()==0)
+			return true;
+		int num = m.getElement(0);
+		int next = m.getElement(1);
+		boolean isTrue = num >= next;
+		if(isTrue){
+			m.removeElement(0);
+			isReverse(m);
+			m.addElement(0,num);
+		}
+		else
+			isTrue = false;
+		return isTrue;
+	}
+
+	//-------------------------------------------------------------------
+	// 3. getNumAppearances --> Computes the amount of times that integer appears in MyList  
+	//-------------------------------------------------------------------	
+	/**
+	 * The function computes the amount of times that the integer n appears in m.   
+	 * @param m: The MyList we want to use.
+	 * @param n: The number we want to compute its appearances for.
+	 * @return: The amount of appearances of n into m  
+	 */	
+	public int getNumAppearances(MyList<Integer> m, int n){
+		int count = 0;
+		int size = m.length();
+		if(size > 0)
+			for(int i=0; i<size;i++)
+				count = n == m.getElement(i) ? count + 1:count;
+		return count;
+	}
+	
+	//-------------------------------------------------------------------
+	// 4. power --> Computes the m-est power of n
+	//-------------------------------------------------------------------	
+	/**
+	 * The function computes n to the power of m.
+	 * @param n: The base number.
+	 * @param m: The power of n we want to compute
+	 * @return: n to the power of m.  
+	 */	
+
+	public int power(int n, int m){
+		return (int)Math.pow(n, m);
+	}
+	
+	//-------------------------------------------------------------------
+	// 5. lucas --> Computes the n-est term of the Lucas series
+	//-------------------------------------------------------------------	
+	/**
+	 * The function computes the n-est term of the Lucas series
+	 * @param n: The n-est term of the series we want to compute
+	 * @return: The term being computed 
+	 */	
+	public int lucas(int n){
+		return -1;
+	}
+
+	//-------------------------------------------------------------------
+	// 6. drawImage --> Prints a pattern of a given length
+	//-------------------------------------------------------------------	
+	/**
+	 * The function prints prints a pattern of a given length.
+	 * *
+	 * **
+	 * ***
+	 * ... 
+	 * @param n: The length of the desired pattern
+	 */	
+	public void drawImage(int n){
+//		String str = "";
+//		for(int i=n; i>0; i--)
+//			str += "*";
+//		str = str + "\n" + drawImage(n--);
+	}
+		
+}
